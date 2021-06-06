25077840a39a8b9ebc27a3cfb7841ca3f20465f4
maknetaRo <makneta@gmail.com>
2021-06-06 11:53:57 +0200

    created basic news App with public API

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+  FlatList,
+  Image,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const apiToken = '';
+  const query = 'weather';
+  const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiToken}`;
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setData(json.articles))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  console.log(data);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>News Feed</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => {
+            console.log(item.url);
+            return (
+              <View style={styles.article}>
+                <Image style={styles.image} source={{ uri: item.urlToImage }} />
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.text}>{item.description}</Text>
+                <Text style={styles.author}>Author: {item.author}</Text>
+              </View>
+            );
+          }}
+        />
+      )}
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingTop: 20,
+    marginTop: 30,
+  },
+  header: {
+    fontSize: 26,
+  },
+  article: {
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    width: '100%',
+    marginTop: 20,
+  },
+  title: {
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderTopLeftRadius: 35,
+    borderTopRightRadius: 35,
+
+    fontSize: 20,
+    backgroundColor: '#fff',
+    marginTop: -36,
+    marginLeft: 20,
+    marginRight: 20,
+    zIndex: 1,
+  },
+
+  image: {
+    width: '100%',
+    height: 168,
+    borderTopLeftRadius: 35,
+    borderTopRightRadius: 35,
+  },
+  text: {
+    fontSize: 18,
+    padding: 16,
+    marginBottom: 36,
+  },
+  author: {
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    textDecorationStyle: 'solid',
+    textDecorationColor: '#000',
   },
 });
+
+export default App;
